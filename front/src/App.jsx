dd138b7fecb9d38c0381c76e9870044bd5d026d8
Ana Clara <130106962+maciel-ana@users.noreply.github.com>
2025-11-04 18:19:09 -0300

    Codificação projetos e inicio do contato.

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import './SASS/main.scss'
 import Eu from './assets/ft-eu.png'
+import AgroTech from './assets/agrotech.png'
+import Otis from './assets/otis.png'
 
 function App() {
   const [ animado, setAnimado ] = useState(false);
@@ -47,7 +49,7 @@ function App() {
       <div className="tecnologias">
         <div className="box">
           <div className="info">
-            <i class="fa-brands fa-html5 icon"></i>
+            <i className="fa-brands fa-html5 icon"></i>
             <p className='tipo'>HMTL5</p>
           </div>
           <div className="nivel-container">
@@ -62,7 +64,7 @@ function App() {
 
         <div className="box">
           <div className="info">
-            <i class="fa-brands fa-css3 icon"></i>
+            <i className="fa-brands fa-css3 icon"></i>
             <p className='tipo'>CSS</p>
           </div>
           <div className="nivel-container">
@@ -77,7 +79,7 @@ function App() {
 
         <div className="box">
           <div className="info">
-            <i class="fa-brands fa-js icon"></i>
+            <i className="fa-brands fa-js icon"></i>
             <p className='tipo'>JavaScript</p>
           </div>
           <div className="nivel-container">
@@ -92,7 +94,7 @@ function App() {
 
         <div className="box">
           <div className="info">
-            <i class="fa-brands fa-react icon"></i>
+            <i className="fa-brands fa-react icon"></i>
             <p className='tipo'>React.js</p>
           </div>
           <div className="nivel-container">
@@ -107,7 +109,7 @@ function App() {
 
         <div className="box">
           <div className="info">
-            <i class="fa-brands fa-sass icon"></i>
+            <i className="fa-brands fa-sass icon"></i>
             <p className='tipo'>SASS</p>
           </div>
           <div className="nivel-container">
@@ -123,7 +125,7 @@ function App() {
 
         <div className="box">
           <div className="info">
-            <i class="fa-brands fa-github icon"></i>
+            <i className="fa-brands fa-github icon"></i>
             <p className='tipo'>Git & GitHub</p>
           </div>
           <div className="nivel-container">
@@ -137,6 +139,98 @@ function App() {
         </div>
 
       </div>
+     </section>
+
+     <section className='projetos'>
+      <div className="titulo-proj">
+        <h3>Projetos em Destaque</h3>
+        <p>Uma seleção de trabalhos que demonstram minha paixão e habilidade</p>
+      </div>
+      <div className="projetos-box">
+        <div className="proj">
+          <div className="imagem-proj">
+            <img src={AgroTech} alt="foto projeto AgroTech" />
+          </div>
+          <div className="descricao">
+            <h4>Site para Monitoramento de Pequenas Produções Agrícolas</h4>
+            <p>Plataforma voltada para pequenos agricultores, com foco em reduzir o desperdício de água e insumos agrícolas por meio da tecnologia. O projeto propõe soluções acessíveis e sustentáveis para otimizar o uso de recursos, aumentar a eficiência das produções e promover práticas mais conscientes no campo.</p>
+            <div className="tecnologias-proj">
+              <span>React.js</span>
+              <span>SASS</span>
+            </div>
+            <div className="botoes-proj">
+              <a href='https://github.com/maciel-ana/AgroTECH-.git' target='_blank' className='github'>Código</a>
+              <a href='https://agro-tech-fawn.vercel.app/' target='_blank' className='site'>Site</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="proj">
+          <div className="imagem-proj">
+            <img src={Otis} alt="foto projeto AgroTech" />
+          </div>
+          <div className="descricao">
+            <h4>Site para Monitoramento de Instalações de Elevadores OTIS</h4>
+            <p>Plataforma desenvolvida em parceria com a Otis, voltada para padronizar e centralizar o acompanhamento das instalações de elevadores em diferentes países da América Latina, promovendo mais eficiência, transparência e integração entre as equipes.</p>
+            <div className="tecnologias-proj">
+              <span>React.js</span>
+              <span>Firebase</span>
+              <span>CSS</span>
+            </div>
+            <div className="botoes-proj">
+              <a href='https://github.com/maciel-ana/otis-mvp.git' target='_blank' className='github'>Código</a>
+              <a href='https://otis-mvp.vercel.app/' target='_blank' className='site'>Site</a>
+            </div>
+          </div>
+        </div>
+    
+        <div className="proj">
+          <div className="imagem-proj">
+            <img src={AgroTech} alt="foto projeto AgroTech" />
+          </div>
+          <div className="descricao">
+            <h4>Site para organização de tarefas</h4>
+            <p>O DOIT é um projeto pessoal de aplicação web focado em produtividade, desenvolvido como uma lista de tarefas (to-do list) interativa. O objetivo principal da ferramenta é oferecer uma interface limpa e intuitiva para que o usuário possa cadastrar, organizar e gerenciar suas atividades e pendências diárias de forma eficiente.</p>
+            <div className="tecnologias-proj">
+              <span>HTML</span>
+              <span>CSS</span>
+              <span>JavaScript</span>
+              <span>Firebase</span>
+            </div>
+            <div className="botoes-proj">
+              <a href='https://github.com/maciel-ana/DOIT.git' target='_blank' className='github'>Código</a>
+              <a href='https://agro-tech-fawn.vercel.app/' target='_blank' className='site'>Site</a>
+            </div>
+          </div>
+        </div>
+      </div>
+     </section>
+
+     <section className='contato'>
+        <div className="conteudo-ctt">
+          <h2>Vamos Conversar</h2>
+          <p>Tem um projeto em mente ou quer se conectar? Me envie uma mensagem!</p>
+
+          <form className='forms'>
+            <label>Nome</label>
+            <input type="text" name='email'/>
+
+            <label>Email</label>
+            <input type="email" name='email'/>
+
+            <label>Mensagem</label>
+            <textarea name="mensagem" id="msg"></textarea>
+
+            <div className="botao">
+              <button className='btn-enviar' type='submit'>Enviar Mensagem</button>
+            </div>
+
+            <div className="redes">
+              <i className="fa-brands fa-github icon"></i>
+              <i class="fa-brands fa-linkedin icon"></i>
+            </div>
+          </form>
+        </div>
      </section>
     </>
   )
